9b29cf66e46c13a70a0e9bb9ea4f351909e053a4
EdwinKimsal <107333344+EdwinKimsal@users.noreply.github.com>
2023-06-16 11:50:39 -0400

    Add files via upload

--- Searchifyy/code.js
+++ Searchifyy/code.js
@@ -1,46 +1,90 @@
-// Search Algorithm
+// search algorithm
 function score(){
-    // Variables
-    var input = document.getElementById('searchArea').value.toLowerCase();
+    // variables
+    var query = document.getElementById('searchArea').value.toLowerCase();
     var output = document.getElementById('result');
+    var booleanCountArray = [];
+    var totalCountArray = [];
     var newTitleArray = [];
     var newUrlArray = [];
+    var queryArray = [];
+    var countFrequency = [];
     var countArray = [];
+    var totalCount = 0;
     var count = 0;
     output.innerHTML = '';
 
-    // Copying urlArray to newUrlArray
+    // copying arrays into newArrays
     for (var i = 0; i < urlArray.length; i++){
         newTitleArray.push(titleArray[i]);
         newUrlArray.push(urlArray[i]);
     }
 
-    if (input != ''){
-        // Creating Scores
-        for (var i = 0; i < htmlArray.length; i++){
-            var re = new RegExp(input, 'gi');
-            count = (htmlArray[i].match(re) || []).length;
+    // transforming query into queryArray
+    queryArray = query.split(" ");
 
-            countArray.push(count);
-        }
-
-        for (var i = 0; i < countArray.length; i++){
-            if (newUrlArray[i].includes(input) == true || newTitleArray[i].includes(input) == true){
-                countArray[i] = countArray[i] * 2;
+    // checking if query IS NOT blank
+    if (queryArray != []){
+        // creating scores
+        for (var i = 0; i < newUrlArray.length; i++){
+            // checking each word individualy
+            for (var j = 0; j < queryArray.length; j++){
+                var re = new RegExp(queryArray[j], 'g');
+                count = (htmlArray[i].match(re) || []).length;
+            
+                // resetting count
+                countArray.push(count);
+                count = 0;
             }
         }
 
-        // Greatest Score to Lowest Score
+        // adding content to booleanCountArray
         for (var i = 0; i < countArray.length; i++){
-            for (var j = 0; j < countArray.length; j++){
-                if (countArray[j] < countArray[j + 1]){
-                    var tempCount = countArray[j];
-                    countArray[j] = countArray[j + 1];
-                    countArray[j + 1] = tempCount;
+            if (countArray[i] > 0){
+                booleanCountArray.push(1);
+            }
+
+            else{
+                booleanCountArray.push(0);
+            }
+        }
+
+        // adding content to countFrequency
+        for (var i = 0; i < queryArray.length; i++){
+            countFrequency.push(0);
+        }
+
+        // getting frequency of each word per page
+        for (var i = 0; i < countFrequency.length; i++){
+            for (var j = 0; j < booleanCountArray.length/countFrequency.length; j++){
+                countFrequency[i] += booleanCountArray[((countFrequency.length * j) + i)];
+            }
+        }
+
+        // updating the final scores for each page
+        for (var i = 0; i < newUrlArray.length; i++){
+            for (var j = 0; j < queryArray.length; j++){
+                if (countFrequency[j] != 0){
+                    totalCount += countArray[((queryArray.length * i) + j)] / countFrequency[j];
+                }
+            }
+
+            // pushing/resetting totalCount
+            totalCountArray.push(totalCount);
+            totalCount = 0;
+        }
+        
+        // greatest score to lowest score
+        for (var i = 0; i < totalCountArray.length; i++){
+            for (var j = 0; j < totalCountArray.length; j++){
+                if (totalCountArray[j] < totalCountArray[j + 1]){
+                    var tempCount = totalCountArray[j];
+                    totalCountArray[j] = totalCountArray[j + 1];
+                    totalCountArray[j + 1] = tempCount;
 
                     var tempTitle = newTitleArray[j];
                     newTitleArray[j] = newTitleArray[j + 1];
-                    newTitleArray[j + 1] = tempTitle
+                    newTitleArray[j + 1] = tempTitle;
 
                     var tempUrl = newUrlArray[j];
                     newUrlArray[j] = newUrlArray[j + 1];
@@ -49,9 +93,9 @@ function score(){
             }
         }
 
-        // Showing Results
+        // showing results
         for (var i = 0; i < newUrlArray.length; i++){
-            if (countArray[i] > 0){
+            if (totalCountArray[i] > 0){
                 // Set output paragraph's HTML to the index array
                 const section = document.createElement('section')
                 const link = document.createElement('a');
@@ -65,7 +109,7 @@ function score(){
         }
     }
 
-    // Result if query is blank
+    // reset output if query is blank
     else{
         output.innerHTML = ''
     }
